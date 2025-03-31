1d92ba8d9fdd062cbacbd1159ca99e35ce2edcb9
stack2284 <sahilshaikh220804@gmail.com>
2025-03-31 19:13:29 +0530

    Updated flappy_ai.py to improve game mechanics and AI behavior. updated readme.md

--- flappy_ai.py
+++ flappy_ai.py
@@ -1,10 +1,7 @@
 """
-The classic game of flappy bird. Make with python
-and pygame. Features pixel perfect collision using masks :o
-
-Date Modified:  Jul 30, 2019
-Author: Tech With Tim
-Estimated Work Time: 5 hours (1 just for that damn collision)
+The classic game of flappy bird;
+Made with python3, neat-python, pygame; 
+Features pixel perfect collision using masks :o
 """
 import pygame
 import random
